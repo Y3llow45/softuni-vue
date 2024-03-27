acd07ab076f8c2fd295c3d6360adcff73e41bcb9
Borislav Hristov <bofengejn@gmail.com>
2024-03-27 19:10:04 +0200

    create image-gallery

---
+++ image-gallery/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h2>Image Gallery</h2>
+    <img v-bind:src="imageURL" style="width: 200px;" />
+    <a v-bind:href="imageURL">Go to site</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      imageURL: 'https://i.natgeofe.com/n/506bcd08-37e0-4506-b4cc-328b028cdd8c/NationalGeographic_2683370_2x3.jpg'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
