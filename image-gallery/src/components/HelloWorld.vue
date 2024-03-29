25ee5f62c972c41f321416e3bad99961fe05634f
Borislav Hristov <bofengejn@gmail.com>
2024-03-29 19:05:04 +0200

    List rendering

--- image-gallery/src/components/HelloWorld.vue
+++ image-gallery/src/components/HelloWorld.vue
@@ -12,7 +12,12 @@
 
     <h2 v-on:mouseover="showHeading = true">Time is ...?</h2>
     <div v-if="showHeading">{{currentDate}}</div>
-  </div>
+
+    <div v-for="f in fruits" v-bind:key="f.id">
+      <h2 v-bind:key="fruit">{{f.type}}</h2>
+      <p v-bind:key="fruit">{{f.weight}}</p>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -23,7 +28,13 @@ export default {
       imageURL: 'https://i.natgeofe.com/n/506bcd08-37e0-4506-b4cc-328b028cdd8c/NationalGeographic_2683370_2x3.jpg',
       isValid: true,
       currentDate: new Date(),
-      showHeading: false
+      showHeading: false,
+      fruits: [
+        {id: 0, type: 'Kiwi', weight: 0.5},
+        {id: 1, type: 'Banana', weight: 1},
+        {id: 2, type: 'Orange', weight: 1.5},
+        {id: 3, type: 'Apple', weight: 1},
+      ]
     }
   }
 }
