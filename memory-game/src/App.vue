e38dfcad730517b383b8ed4f873825af11ec02f2
Borislav Hristov <bofengejn@gmail.com>
2024-03-31 13:50:58 +0300

    Add Card component

--- memory-game/src/App.vue
+++ memory-game/src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="start" class="btn-start">Play memo game</button>
+    <img src="Next.png" :class="{ 'card': true, 'flipped': isFlipped }" />
+    <div class="grid">
+      <Card v-for="c in 16" :key="c" :c="c" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
+  },
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      isFlipped: false
+    };
+  },
+  methods: {
+    start() {
+      this.isFlipped = !this.isFlipped;
+    }
   }
 }
 </script>
@@ -24,5 +40,31 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn-start {
+  width: 200px;
+  height: 50px;
+  background-color: #ffffffff;
+  border: 1px solid rgb(255, 190, 24);
+  border-radius: 4px;
+  cursor: pointer;
+  color: black;
+  font-family:Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 20px;
+  font-weight: 100;
+  text-decoration: none;
+  transition-duration: 0.4s;
+  text-align: center;
+  margin-top: 100px;
+}
+.btn-start:hover {
+  background-color: rgb(255, 190, 24);
+  color: white;
+}
+.card {
+  width: 200px;
+}
+.flipped {
+  transform: rotateY(180deg);
 }
 </style>
---
+++ memory-game/src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="start">
+    <h1>{{c}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card-img',
+  props: {
+    c: Number,
+  },
+  data() {
+    return {
+      isFlipped: false,
+      imageSRC: {
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+      }
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  
+</style>
