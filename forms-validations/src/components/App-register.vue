981f1ffa19452c5af4f7c7b05045e68af83ae188
Borislav Hristov <bofengejn@gmail.com>
2024-05-01 15:09:04 +0300

    Add validations

--- forms-validations/src/components/App-register.vue
+++ forms-validations/src/components/App-register.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>
+  <form @submit.prevent="submitHandler">
     <div class="form-group">
       <label for="username">Username</label>
-      <input id="username" />
+      <input id="username" v-model="username"/>
+      <div v-if="$v.username.$error">Error</div>
     </div>
-  </div>
+    <button>Register</button>
+  </form>
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate';
+import { required, minLength } from 'vuelidate/lib/validators';
+
 export default {
-  
+  mixins: [validationMixin],
+  data() {
+    return {
+      username: ''
+    }
+  },
+  validations: {
+    username: {
+      required,
+      minLength: minLength(5)
+    }
+  },
+  methods: {
+    submitHandler() {
+      this.$v.$touch();
+      if(this.$v.$invalid) return;
+      console.log('Form was submitted!');
+    }
+  }
 }
 </script>
