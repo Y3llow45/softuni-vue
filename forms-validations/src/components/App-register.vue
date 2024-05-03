454e52f1bd6899610b6a4e1e29d049b5b5dc2895
Borislav Hristov <bofengejn@gmail.com>
2024-05-03 17:27:31 +0300

    Update App-register.vue

--- forms-validations/src/components/App-register.vue
+++ forms-validations/src/components/App-register.vue
@@ -2,10 +2,25 @@
   <form @submit.prevent="submitHandler">
     <div class="form-group">
       <label for="username">Username</label>
-      <input id="username" v-model="username"/>
+      <input id="username" v-model="username" @blur="$v.username.$touch" />
       <template v-if="$v.username.$error">
         <div v-if="!$v.username.required">Username is required</div>
         <div v-else-if="!$v.username.minLength">Username should be atleast 5 characters long</div>
+      </template>
+    </div>
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input id="password" v-model="password" @blur="$v.password.$touch" />
+      <template v-if="$v.password.$error">
+        <div v-if="!$v.password.required">Password is required</div>
+        <div v-else-if="!$v.password.minLength">Password should be atleast 8 characters long</div>
+      </template>
+    </div>
+    <div class="form-group">
+      <label for="cPassword">Confirm password</label>
+      <input id="cPassword" v-model="cPassword" @blur="$v.cPassword.$touch" />
+      <template v-if="$v.cPassword.$error">
+        <div v-if="!$v.cPassword.sameAs">Passwords don't match</div>
       </template>
     </div>
     <button>Register</button>
@@ -16,17 +31,32 @@
 import { validationMixin } from 'vuelidate';
 import { required, minLength } from 'vuelidate/lib/validators';
 
+function sameAs(field) {
+  return function (value) {
+    return this[field] === value;
+  }
+}
+
 export default {
   mixins: [validationMixin],
   data() {
     return {
-      username: ''
+      username: '',
+      password: '',
+      cPassword: ''
     }
   },
   validations: {
     username: {
       required,
       minLength: minLength(5)
+    },
+    password: {
+      required,
+      minLength: minLength(8)
+    },
+    cPassword: {
+      sameAs: sameAs('password')
     }
   },
   methods: {
