84648fe196ca4bba868a7f5992c74da41ddf31b2
Borislav Hristov <bofengejn@gmail.com>
2024-05-08 18:01:10 +0300

    Update HelloWorld.vue

--- workshop-validation/src/components/HelloWorld.vue
+++ workshop-validation/src/components/HelloWorld.vue
@@ -8,26 +8,32 @@
       <label for="username"><span><i class="fas fa-user"></i></span></label>
       <input type="text" name="username" id="username" class="error" placeholder="Mark Ulrich" v-model="username" @blur="$v.username.$touch">
     </p>
-
-    <!-- if error -->
     <template v-if="$v.username.$error && $v.username.$dirty">
-      <p class="error" v-if="!$v.username.required">Full name field is required!</p>
-      <p class="error" v-else-if="!$v.username.custom">Full name field is invalid!</p>
+      <p class="error" v-if="!$v.username.required">Full name is required!</p>
+      <p class="error" v-else-if="!$v.username.custom">Full name is invalid!</p>
     </template>
-    <!-- end if error -->
     
     <p class="field field-icon">
       <label for="email"><span><i class="fas fa-envelope"></i></span></label>
-      <input type="text" name="email" id="email" placeholder="marg@gmial.com">
+      <input type="text" name="email" id="email" placeholder="marg@gmial.com" v-model="email" @blur="$v.email.$touch">
     </p>
+    <template v-if="$v.email.$error && $v.email.$dirty">
+      <p class="error" v-if="!$v.email.required">Email is required!</p>
+      <p class="error" v-else-if="!$v.email.custom">Email is invalid!</p>
+    </template>
 
     <p class="field field-icon">
       <label for="tel"><span><i class="fas fa-phone"></i></span></label>
       <select name="tel" id="tel" class="tel">
         <option value="1">+359</option>
       </select>
-      <input type="text" name="tel" id="tel" placeholder="888 888">
+      <input type="text" name="tel" id="tel" placeholder="8888 8888" v-model="telephone" @blur="$v.telephone.$touch">
     </p>
+    <template v-if="$v.telephone.$error && $v.telephone.$dirty">
+      <p class="error" v-if="!$v.telephone.required">Telephone is required!</p>
+      <p class="error" v-else-if="!$v.telephone.custom">Telephone is invalid!</p>
+      <p class="error" v-else-if="!$v.telephone.maxLength">Telephone is too long!</p>
+    </template>
 
     <p class="field field-icon">
       <label for="building"><span><i class="fas fa-building"></i></span></label>
@@ -42,13 +48,21 @@
 
     <p class="field field-icon">
       <label for="password"><span><i class="fas fa-lock"></i></span></label>
-      <input type="password" name="password" id="password" placeholder="******">
+      <input type="password" name="password" id="password" placeholder="******"  v-model="password" @blur="$v.password.$touch">
     </p>
+    <template v-if="$v.password.$error && $v.password.$dirty">
+      <p class="error" v-if="!$v.password.required">Password is required!</p>
+      <p class="error" v-else-if="!$v.password.minLength">Password is invalid!</p>
+    </template>
 
     <p class="field field-icon">
       <label for="re-password"><span><i class="fas fa-lock"></i></span></label>
-      <input type="re-password" name="re-password" id="re-password" placeholder="******">
+      <input type="re-password" name="re-password" id="re-password" placeholder="******"  v-model="cPassword" @blur="$v.cPassword.$touch">
     </p>
+    <template v-if="$v.cPassword.$error && $v.cPassword.$dirty">
+      <p class="error" v-if="!$v.cPassword.required">Password is required!</p>
+      <p class="error" v-else-if="!$v.cPassword.sameAs">Passwords don't match!</p>
+    </template>
 
     <p>
       <button :disabled="$v.$invalid" v-on="submitHandler()">Create Account</button>
@@ -66,9 +80,11 @@
 
 <script>
 import { validationMixin } from 'vuelidate';
-import { required, minLength, sameAs } from 'vuelidate/lib/validators';
+import { maxLength } from 'vuelidate/lib/validators';
+import { required, minLength, sameAs, email } from 'vuelidate/lib/validators';
 
 const usernamePattern = /[A-Z]{1}[a-z]{2,10} [A-Z]{1}[a-z]{2,10}/s;
+const telephonePattern = /\d{8}/s;
 
 export default {
   mixins: [validationMixin],
@@ -87,11 +103,21 @@ export default {
       required,
       custom: value => usernamePattern.test(value)
     },
+    email : {
+      required,
+      email
+    },
+    telephone: {
+      required,
+      custom: value => telephonePattern.test(value),
+      maxLength: maxLength(8)
+    },
     password: {
       required,
       minLength: minLength(8)
     },
     cPassword: {
+      required,
       sameAs: sameAs('password')
     }
   },
