fc13c6dd53082755051cce7c2be33493c715c096
Borislav Hristov <bofengejn@gmail.com>
2024-05-09 19:56:35 +0300

    add mixin

---
+++ filters-mixins-http/src/mixins/testMixin.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  },
+  //data() {
+  //  return {
+  //    someText: this.text
+  //  }
+  //},
+  computed: {
+    textLength() {
+      return this.text.length;
+    }
+  }
+}
+</script>
