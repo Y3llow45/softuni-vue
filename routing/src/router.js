ef60731e0b15453a02103d4d30f07330f8a69e07
Borislav Hristov <bofengejn@gmail.com>
2024-05-17 11:59:38 +0300

    Basic Routing

---
+++ routing/src/router.js
@@ -0,0 +1,19 @@
+import VueRouter from "vue-router";
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: HomePage,
+    },
+    {
+      path: '/about',
+      component: AboutPage
+    }
+  ]
+})
+
+export default router;
